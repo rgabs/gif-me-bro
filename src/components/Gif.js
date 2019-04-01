558317f75e039e226e3783819427f7d0cf77f806
rgabs <rahulgaba16@gmail.com>
2019-04-02 00:32:11 +0530

    Added zoom in animation

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,30 +1,24 @@
 import React, { useState} from 'react';
+import ImageZoom from 'react-medium-image-zoom'
 
 const Gif = ({ gifUrl, stillUrl, title, aspectRatio, width }) => {
     const [currentUrl, setCurrenturl] = useState(gifUrl);
-    const [fullScreenStatus, setFullScreenStatus] = useState(false);
 
     const style = {
-        width: fullScreenStatus ? "100%" : width,
-        height: fullScreenStatus ? "100%" : width / aspectRatio
+        width: width,
+        height: width / aspectRatio
     };
 
     const onImagePress = () => {
         setCurrenturl(currentUrl === gifUrl ? stillUrl : gifUrl);
-    }
-
-    const toggleFullScreen = () => {
-        setFullScreenStatus(!fullScreenStatus);
-        setCurrenturl(gifUrl);
-    }
+    };
 
     return (
-        <div style={style} className={`image-container ${fullScreenStatus ? "full-screen" : ""}`}>
+        <div style={style} className={`image-container`}>
             <div className="video-controls">
                 <span onClick={onImagePress} className={`control-icon ${currentUrl === gifUrl ? "pause" : "play"}`}></span>
-                <span onClick={toggleFullScreen} className={`control-icon ${fullScreenStatus ? "collapse" : "expand"}`} ></span>
             </div>
-            <img src={currentUrl} alt={title} width="100%" />
+            <ImageZoom image={{ src: currentUrl }} alt={title} defaultStyles={{image: {width: '100%'}}}/>
             <br />
         </div>
     )
