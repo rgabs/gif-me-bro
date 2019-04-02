57f16e785b3e61d71271d848d13c2650a7f300cb
rgabs <rahulgaba16@gmail.com>
2019-04-02 23:55:25 +0530

    Updates

---
+++ precache-manifest.707b5ca4a2a0bd7629217abba84d18a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "663874067f2250769f64",
+    "url": "./static/css/main.b28b30c7.chunk.css"
+  },
+  {
+    "revision": "663874067f2250769f64",
+    "url": "./static/js/main.1823daba.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7beeed774648b4afe15a",
+    "url": "./static/js/2.a75013ab.chunk.js"
+  },
+  {
+    "revision": "8bdc4707c77ff141d4040209e84e3711",
+    "url": "./static/media/picture.8bdc4707.svg"
+  },
+  {
+    "revision": "6f5ba3211fe31c6377037f731c68c381",
+    "url": "./static/media/play.6f5ba321.svg"
+  },
+  {
+    "revision": "42409c09644e4bbfdcb350b5eea471d7",
+    "url": "./static/media/pause.42409c09.svg"
+  },
+  {
+    "revision": "f6b5273532658e644cf76ab6be0b8672",
+    "url": "./index.html"
+  }
+];
